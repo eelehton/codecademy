d16001e87a33a20148687f621d7ef802ff027532
Liz L <liz.lehtonen@gmail.com>
2018-04-17 23:11:15 -0400

    Update Business.js

--- ravenous/src/components/Business/Business.js
+++ ravenous/src/components/Business/Business.js
@@ -16,20 +16,20 @@ let business = {
 class Business extends React.Component {
   render() {
     return (
-      <div className={Business.name}>
+      <div className={business.name}>
   <div className="image-container">
     <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
   </div>
   <h2>MarginOtto Pizzeria</h2>
-  <div className={Business.category}>
-    <div className={Business.address}>
+  <div className={business.category}>
+    <div className={business.address}>
       <p>1010 Paddington Way</p>
       <p>Flavortown</p>
       <p>NY 10101</p>
     </div>
-    <div className={Business.reviewCount}>
+    <div className={business.reviewCount}>
       <h3>ITALIAN</h3>
-      <h3 className={Business.rating}>4.5 stars</h3>
+      <h3 className={business.rating}>4.5 stars</h3>
       <p>90 reviews</p>
     </div>
   </div>
